0507dd80066c055ad9f48d310d373865e9a567d4
voron_76 <voron_76@mail.ru>
2022-12-28 10:34:24 +0300

    v.4.0R. Add print by parts

--- js/print/Print.js
+++ js/print/Print.js
@@ -6,36 +6,58 @@ export default class Print {
 
         <form class="form mt-4" id="label-form">
             <div class="field is-grouped">
-                <div class="field">
+                <div class="field mr-4">
                     <label class="label">Издание</label>
                     <div class="select">
                         <select id="select-title" name="title"></select>
                     </div>
                     <p class="help is-danger is-hidden" id="title-warning">Заполните поле</p>
                 </div>
-                <div class="field ml-4">
+                <div class="field mr-4">
+                    <label class="label">Печатать</label>
+                    <div class="control">
+                        <label class="radio">
+                            <input type="radio" name="printall" value="full" checked>
+                            Всё
+                        </label>
+                        <label class="radio">
+                            <input type="radio" name="printall" value="byparts">
+                            Частями
+                        </label>
+                    </div>
+                </div>
+                <div class="field mr-4">
+                    <label class="label">Часть</label>
+                    <div class="select">
+                        <select id="select-part" name="part" disabled></select>
+                    </div>
+                    <p class="help is-danger is-hidden" id="part-warning">Заполните поле</p>
+                </div>
+            </div>
+            <div class="field is-grouped">
+                <div class="field mr-4">
                     <label class="label">Номер</label>
                     <input name="num" class="input" type="text" placeholder="Номер">
                     <p class="help is-danger is-hidden" id="num-warning">Заполните поле</p>
                 </div>
-                <div class="field ml-4">
+                <div class="field mr-4">
                     <label class="label">Дата выхода</label>
                     <input name="date" class="input" type="date" placeholder="Дата">
                     <p class="help is-danger is-hidden" id="date-warning">Заполните поле</p>
                 </div>
-                <div class="field ml-4">
+                <div class="field mr-4">
                     <label class="label">Тираж</label>
                     <input name="count" class="input" type="number" placeholder="Тираж">
                     <p class="help is-danger is-hidden" id="count-warning">Заполните поле</p>
                 </div>
-                <div class="field ml-4">
+                <div class="field mr-4">
                     <label class="label">Номер заказа</label>
                     <input name="ordernum" class="input" type="string" placeholder="Номер заказа">
                     <p class="help is-danger is-hidden" id="ordernum-warning">Заполните поле</p>
                 </div>
             </div>
             <div class="field is-grouped">
-                <div class="field ml-4">
+                <div class="field mr-4">
                     <label class="label">Формат</label>
                     <div class="control">
                         <label class="radio">
@@ -49,7 +71,7 @@ export default class Print {
                     </div>
                     <p class="help is-danger is-hidden" id="size-warning">Заполните поле</p>
                 </div>
-                <div class="field ml-4">
+                <div class="field mr-4">
                     <label class="label">Количество в пачке</label>
                     <input name="countpack" class="input" type="number" placeholder="В пачке" value="200" step=50>
                     <p class="help is-danger is-hidden" id="count-pack-warning">Заполните поле</p>
@@ -91,7 +113,7 @@ export default class Print {
         this.view.querySelector('#date-warning').classList.remove('is-hidden');
         flag = false;
       };
-      if(form.count.value == '') {
+      if(((form.printall.value == 'full')) && (form.count.value == '')) {
         this.view.querySelector('#count-warning').classList.remove('is-hidden');
         flag = false;
       };
@@ -101,6 +123,10 @@ export default class Print {
       };
       if(form.size.value == '') {
         this.view.querySelector('#size-warning').classList.remove('is-hidden');
+        flag = false;
+      };
+      if((form.printall.value == 'byparts') && (form.part.value == '')) {
+        this.view.querySelector('#part-warning').classList.remove('is-hidden');
         flag = false;
       };
       return flag;
@@ -119,9 +145,17 @@ export default class Print {
         }
       } else {
         this.selectTitle.innerHTML = `<option value=0>${this.labelData[0].title}</option>`;
+        this.renderPartSelect(this.labelData[0]);
         if(this.labelData[0].totalCount) {
-          this.labelForm.elements.count.value = this.labelData[0].totalCount;
+          this.labelForm.elements.count.value = this.labelData[0].totalCount;          
         };
+      }
+    }
+
+    renderPartSelect(data) {
+      this.selectPart.innerHTML = `<option></option>`;
+      for(let key in data.data) {
+        this.selectPart.innerHTML += `<option value=${key}>${data.data[key].title ? data.data[key].title : '--Пустой--'}</option>`;
       }
     }
 
@@ -143,11 +177,12 @@ export default class Print {
           setTimeout(()=> {
               WinPrint.print();
               WinPrint.close();
-              this.labelForm.reset();
+              // this.labelForm.reset();
           },3000);
     }
 
     generateLabels() {
+      console.log(this.printParams);
       this.requestRenderLabels()
           .then(result => {
               this.print(result);
@@ -157,15 +192,53 @@ export default class Print {
     injection(key, obj) {
       this[key] = obj;
     }
+
+    setPrintMode(flag) {
+      switch (flag) {
+        case 'byparts':
+          this.selectPart.removeAttribute('disabled');
+          this.view.querySelector('[name="count"]').setAttribute('disabled', 'disabled');
+          this.fullFlag = false;
+          break;
+        case 'full':
+          this.printPart = {};
+          this.selectPart.setAttribute('disabled', 'disabled');
+          this.view.querySelector('[name="count"]').removeAttribute('disabled');
+          this.fullFlag = true;
+          break;
+      }
+    }
+
+    setPrintPart(data) {
+      this.printData = data;
+    }
     
     constructor() {
 
       this.view = document.createElement('div');
       this.view.innerHTML = this.template;
+      this.fullFlag = true;
 
       this.selectTitle = this.view.querySelector('#select-title');
+      this.selectPart = this.view.querySelector('#select-part');
       this.btnPrint = this.view.querySelector('#button-print');
+      
       this.labelForm = this.view.querySelector('#label-form');
+      this.printAll = this.view.querySelectorAll('[name="printall"]');
+
+      this.printAll.forEach(button => {
+        button.addEventListener('click', (e) => {
+          this.setPrintMode(e.target.value);
+        });
+      });
+
+      this.selectTitle.addEventListener('change', (e) => {
+        this.renderPartSelect(this.labelData[e.target.value]);
+      })
+
+      this.selectPart.addEventListener('change', (e) => {
+        this.setPrintPart(this.labelData[this.labelForm.elements.title.value].data[e.target.value]);
+      })
 
       this.btnPrint.addEventListener('click', (e) => {
         e.preventDefault();
@@ -176,10 +249,10 @@ export default class Print {
             num: this.labelForm.elements.num.value,
             orderNum: this.labelForm.elements.ordernum.value,
             date: this.formatDate(this.labelForm.elements.date.value),
-            totalCount: this.labelForm.elements.count.value,
+            totalCount: this.fullFlag ? this.labelForm.elements.count.value : this.printData.value,
             maxItemsInPack: this.labelForm.elements.countpack.value,
             size: this.labelForm.elements.size.value,
-            data: this.labelData[this.labelForm.elements.title.value].data
+            data: this.fullFlag ? this.labelData[this.labelForm.elements.title.value].data : [this.printData]
           };
           this.generateLabels();
         }        
